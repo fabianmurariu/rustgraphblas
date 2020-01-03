4e35fd9c80b911cb393f52619a5f2379a658100c
FabianM <murariu.fabian@gmail.com>
2020-01-03 17:32:41 +0000

    added lazy GrB_Init call

--- src/lib.rs
+++ src/lib.rs
@@ -6,12 +6,12 @@ include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 use std::mem::{MaybeUninit};
 use enum_primitive::*;
 use std::marker::PhantomData;
-// #[macro_use]
-// extern crate lazy_static;
+#[macro_use]
+extern crate lazy_static;
 
-// lazy_static! {
-//     static ref GRB:u32 = unsafe {GrB_init(GrB_Mode_GrB_NONBLOCKING)};
-// }
+lazy_static! {
+    static ref GRB:u32 = unsafe {GrB_init(GrB_Mode_GrB_NONBLOCKING)};
+}
 
 enum_from_primitive! {
 #[derive(Debug, Copy, Clone, PartialEq)]
@@ -30,12 +30,12 @@ pub trait TypeEncoder {
     fn blas_type() -> SparseType;
 }
 
-impl TypeEncoder for bool {
-    fn blas_type() -> SparseType {
-        let tpe = unsafe {&mut GrB_BOOL as *mut GrB_Type} ;
-        SparseType{tpe: tpe}
-    }
-}
+// impl TypeEncoder for bool {
+//     fn blas_type() -> SparseType {
+//         let tpe = unsafe {&mut GrB_BOOL as *mut GrB_Type} ;
+//         SparseType{tpe: tpe}
+//     }
+// }
 
 pub struct SparseMatrix<T>{
     mat: GrB_Matrix,
@@ -44,7 +44,8 @@ pub struct SparseMatrix<T>{
 
 impl<T:TypeEncoder> SparseMatrix<T> {
     pub fn empty(size: (u64, u64)) -> SparseMatrix<T>{
-
+        let _ = *GRB;
+       
         let mut A = MaybeUninit::<GrB_Matrix>::uninit();
 
         let (rows, cols) = size;
@@ -54,7 +55,7 @@ impl<T:TypeEncoder> SparseMatrix<T> {
                     let mat = A.as_mut_ptr();
                     SparseMatrix{ mat: *mat, _marker: PhantomData , }
                 },
-                err => panic!("Failed to init matrix ")
+                e => panic!("Failed to init matrix GrB_error {}", e)
             }
         }
 
@@ -88,6 +89,23 @@ impl<T> Drop for SparseMatrix<T> {
 
 make_matrix_like!(bool, GrB_Matrix_extractElement_BOOL, GrB_Matrix_setElement_BOOL);
 make_matrix_like!(i8, GrB_Matrix_extractElement_INT8, GrB_Matrix_setElement_INT8);
+make_matrix_like!(u8, GrB_Matrix_extractElement_UINT8, GrB_Matrix_setElement_UINT8);
+make_matrix_like!(i16, GrB_Matrix_extractElement_INT16, GrB_Matrix_setElement_INT16);
+make_matrix_like!(u16, GrB_Matrix_extractElement_UINT16, GrB_Matrix_setElement_UINT16);
+make_matrix_like!(i32, GrB_Matrix_extractElement_INT32, GrB_Matrix_setElement_INT32);
+make_matrix_like!(u32, GrB_Matrix_extractElement_UINT32, GrB_Matrix_setElement_UINT32);
+make_matrix_like!(i64, GrB_Matrix_extractElement_INT64, GrB_Matrix_setElement_INT64);
+make_matrix_like!(u64, GrB_Matrix_extractElement_UINT64, GrB_Matrix_setElement_UINT64);
+
+make_base_matrix_type!(bool, GrB_BOOL);
+make_base_matrix_type!(i8, GrB_INT8);
+make_base_matrix_type!(u8, GrB_UINT8);
+make_base_matrix_type!(i16, GrB_INT16);
+make_base_matrix_type!(u16, GrB_UINT16);
+make_base_matrix_type!(i32, GrB_INT32);
+make_base_matrix_type!(u32, GrB_UINT32);
+make_base_matrix_type!(i64, GrB_INT64);
+make_base_matrix_type!(u64, GrB_UINT64);
 
 #[cfg(test)]
 mod tests {
@@ -95,8 +113,6 @@ mod tests {
 
     #[test]
     fn create_bool_sparse_matrix() {
-        unsafe {GrB_init(GrB_Mode_GrB_BLOCKING)};
-
         let mut m = SparseMatrix::<bool>::empty((5, 5));
         // assert!(m.rows() == 5);
         assert!(m.insert(0, 3, true) == ());
@@ -109,6 +125,13 @@ mod tests {
         assert!(m.get(1, 3) == Some(true));
         assert!(m.get(2, 3) == Some(true));
         assert!(m.get(0, 3) == Some(true));
+    }
+
+    #[test]
+    fn create_u64_sparse_matrix() {
+        let mut m = SparseMatrix::<u64>::empty((2, 5));
+        assert!(m.insert(0, 0, 12 as u64) == ());
+        assert!(m.get(0, 0) == Some(12 as u64));
     }
 }
 
@@ -137,6 +160,18 @@ macro_rules! make_matrix_like {
                     }
                 }
 
+            }
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! make_base_matrix_type {
+    ( $typ:ty, $grb_typ:ident ) => {
+        impl TypeEncoder for $typ {
+            fn blas_type() -> SparseType {
+                let tpe = unsafe {&mut $grb_typ as *mut GrB_Type} ;
+                SparseType{tpe: tpe}
             }
         }
     };
